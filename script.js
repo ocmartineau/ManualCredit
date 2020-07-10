d880fd1bac45e6fc47ced5e0ce0dae3164b895f0
Olivier Martineau <omartineau8@gmail.com>
2020-07-09 22:51:23 -0400

    added logic for changing data type for ID and Order ID

--- script.js
+++ script.js
@@ -3,68 +3,24 @@ require('dotenv').config({path: __dirname + '/.env'});
 const csv = require("csvtojson");
 const axios = require("axios");
 
-class Order {
-    constructor(id, notes, order_id, status){
-        this.id = id;
-        this.notes = notes;
-        this.order_id = order_id;
-        this.status = status;
-    }
-   
-}
 
-class CSVtoJsonService {
-    // csvFilePath = './test.csv';
-    constructor(csvFilePath, config) {
-        this.csvFilePath = csvFilePath;
-        this.config = config;
-    }
-    // generateJson() {
-    //     csv().fromFile(this.csvFilePath)
-    //               .then((jsonObj =>{
-    //                 // console.log(jsonObj[0].id)
-    //                 for(let i = 0; i < jsonObj.length; i++){
-    //                     axios({
-    //                         method: 'post',
-    //                         url: 'https://www.refersion.com/api/manual_credit_order_id',
-    //                         data: {
 
-    //                             'id': jsonObj[i].id,
-    //                             'notes': jsonObj[i].notes,
-    //                             'order_id': jsonObj[i].order_id,
-    //                             'status': jsonObj[i].status
-
-    //                         },
-    //                         headers: {
-    //                             'Content-Type': 'application/json',
-    //                             'Refersion-Public-Key': 'pub_f5e8e8370bc7bae15db7',
-    //                             'Refersion-Secret-Key':	'sec_ec9455dce6afa909035b'
-    //                         }
-    //                     })
-    //                 }
-    //             }))
-    // }
-
-    
-}
-
-// class ConversionController { //I don't want to call it a controller
-//     //Has an instance of HTTP Client (makes requestions)
-//     //Has some sort of "#makeRequest" function that accepts either a single Order
-//     //"makeBatchRequest" takes array 
-// }
-
-// const logic = new CSVtoJsonService('./test.csv');
-
-// logic.generateJson();
 
 
 // Testing csv conversion
-const csvFilePath = './manualcommissions.csv';
+const csvFilePath = './manualcredits.csv';
 csv().fromFile(csvFilePath)
      .then((jsonObj =>{
-         console.log(jsonObj)
-     }));
+        //  parseInt(jsonObj[0].id);
+        console.log(jsonObj[0].id);
+        console.log(typeof jsonObj[0].id);
+        let id = Number(jsonObj[0].id);
+        let orderId = Number(jsonObj[0].order_id)
+        console.log(id);
+        console.log(typeof id);
+
+
+}));
 
 
 //Configuring Axios parameters
@@ -90,26 +46,51 @@ csv().fromFile(csvFilePath)
 // });
 
 function orderTest(){
-    axios({
-        method: 'post',
-        url: 'https://www.refersion.com/api/manual_credit_order_id',
-        data: {
+   
+    csv().fromFile(csvFilePath)
+                .then((jsonObj =>{
+                // console.log(jsonObj[0].id)
+                // for(let i = 0; i < jsonObj.length; i++){
+                //     axios({
+                //         method: 'POST',
+                //         url: 'https://www.refersion.com/api/manual_credit_order_id',
+                //         data: {
 
-            'id': '4113082',
-            'notes': 'This is test 1020',
-            'order_id': '1020',
-            'status': 'APPROVED'
+                //             'id': jsonObj[i].id,
+                //             'notes': jsonObj[i].notes,
+                //             'order_id': jsonObj[i].order_id,
+                //             'status': jsonObj[i].status
 
-        },
-        headers: {
-            'Content-Type': 'application/json',
-            'Refersion-Public-Key': process.env.pubKey,
-            'Refersion-Secret-Key':	process.env.secKey
-        }
-    }).then(function(response){
-        console.log(`This request is ${response.statusText} with a status code of ${response.status}`)
-        console.log(response.data)
-    });
+                //         },
+                //         headers: {
+                //             'Content-Type': 'application/json',
+                //             'Refersion-Public-Key': 'pub_f5e8e8370bc7bae15db7',
+                //             'Refersion-Secret-Key':	'sec_ec9455dce6afa909035b'
+                //         }
+                //     })
+                // }
+            }))
+        
+    // axios({
+    //     method: 'post',
+    //     url: 'https://www.refersion.com/api/manual_credit_order_id',
+    //     data: {
+
+    //         'id': '4113082',
+    //         'notes': 'This is test 1020',
+    //         'order_id': '1020',
+    //         'status': 'APPROVED'
+
+    //     },
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //         'Refersion-Public-Key': process.env.pubKey,
+    //         'Refersion-Secret-Key':	process.env.secKey
+    //     }
+    // }).then(function(response){
+    //     console.log(`This request is ${response.statusText} with a status code of ${response.status}`)
+    //     console.log(response.data)
+    // });
 };
 
 function commissionTest(){
@@ -136,5 +117,5 @@ function commissionTest(){
     });
 };
 
-// orderTest();
+orderTest();
 // commissionTest();
