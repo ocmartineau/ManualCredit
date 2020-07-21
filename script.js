b06e8f444ae2dcc06e9494a3d443a17383129fb9
Olivier Martineau <omartineau8@gmail.com>
2020-07-20 21:00:37 -0400

    added functionality for manual commission credit

--- script.js
+++ script.js
@@ -8,52 +8,25 @@ const fs = require('fs');
 
 
 
-// Testing csv conversion
-const csvFilePath = './manualcredits.txt';
-// csv().fromFile(csvFilePath)
-//      .then((jsonObj =>{
-//         //  parseInt(jsonObj[0].id);
-//         console.log(jsonObj[0].id);
-//         console.log(typeof jsonObj[0].id);
-//         let id = Number(jsonObj[0].id);
-//         let orderId = Number(jsonObj[0].order_id)
-//         console.log(id);
-//         console.log(typeof id);
+
+const csvFilePath = '45013BulkManualCommission.csv';
+;
 
 
-// }));
 
+function sendManualOrders(){
 
-//Configuring Axios parameters
-
-// let config = {
-//     headers: {
-//         'Content-Type': 'application/json',
-//         'Refersion-Public-Key':'pub_f5e8e8370bc7bae15db7',
-//         'Refersion-Secret-Key':'sec_ec9455dce6afa909035b'
-//     }
-// };
-
-// let  data = jsonObj;
-
-// axios.post(
-//     'https://www.refersion.com/api/manual_credit_order_id',
-//     data,
-//     config
-// ).then(function(response){
-//     console.log(response);
-// }).catch(function(error){
-//     console.log(error)
-// });
-
-function orderTest(){
-   
+   //Pull CSV and convert to JSON Object...
     csv().fromFile(csvFilePath)
                 .then((jsonObj =>{
                 console.log(jsonObj[0].id)
+
+                //Loop through resulting object to change Order ID and Affiliate Id into the right data type...
                 for(let i = 0; i < jsonObj.length; i++){
                     let id = Number(jsonObj[i].id);
                     let order_id = Number(jsonObj[i].order_id)
+
+                    //Pass resulting correct body format through to Axios...
                     axios({
                         method: 'POST',
                         url: 'https://www.refersion.com/api/manual_credit_order_id',
@@ -67,66 +40,77 @@ function orderTest(){
                         },
                         headers: {
                             'Content-Type': 'application/json',
-                            'Refersion-Public-Key': 'pub_f5e8e8370bc7bae15db7',
-                            'Refersion-Secret-Key':	'sec_ec9455dce6afa909035b'
+                            'Refersion-Public-Key': 'pub_dbc37e60b31cdaa21e11',
+                            'Refersion-Secret-Key':	'sec_76f6cb125e64c4ec22e5'
                         }
+
+                    //Log resulting status to terminal
                     }).then(function(response){
-                        console.log(`For ${order_id} ${response.statusText}`)
+                        console.log(`For ${order_id} the POST is ${response.statusText}`);
+
+                    //Use filesystem to log errors to a text file..    
                     }).catch(function (error) {
-                        console.log(error.response.data);
-                        const results = JSON.stringify(error.response.data) + '\r\n';
-                        fs.appendFile('./testerino/writetest.txt', results, function (err) {
+                        console.log(error.response.config.data);
+                        const results = JSON.stringify(error.response.config.data) + '\r\n';
+                        fs.appendFile('./logs/responselog.txt', `Error ${results}` , function (err) {
                             if (err) throw err;
                         })
                     })
                 }
             }))
         
-    // axios({
-    //     method: 'post',
-    //     url: 'https://www.refersion.com/api/manual_credit_order_id',
-    //     data: {
-
-    //         'id': '4113082',
-    //         'notes': 'This is test 1020',
-    //         'order_id': '1020',
-    //         'status': 'APPROVED'
-
-    //     },
-    //     headers: {
-    //         'Content-Type': 'application/json',
-    //         'Refersion-Public-Key': process.env.pubKey,
-    //         'Refersion-Secret-Key':	process.env.secKey
-    //     }
-    // }).then(function(response){
-    //     console.log(`This request is ${response.statusText} with a status code of ${response.status}`)
-    //     console.log(response.data)
-    // });
+    
 };
 
 function commissionTest(){
-    axios({
-        method: 'post',
-        url: 'https://www.refersion.com/api/manual_commission_credit',
-        data: {
+    //Pull CSV and convert to JSON Object...
+    csv().fromFile(csvFilePath)
+                .then((jsonObj =>{
+                console.log(jsonObj);
+                console.log(jsonObj[0].id);
+                console.log(jsonObj[0].commission);
+                console.log(jsonObj[0].notes);
+                console.log(jsonObj[0].status);
+                console.log(jsonObj[0].currency);
+                // Loop through resulting object to change Commission and Affiliate Id into the right data type...
+                for(let i = 0; i < jsonObj.length; i++){
+                    let id = Number(jsonObj[i].id);
+                    let commission = Number(jsonObj[i].commission)
 
-            'id': '4113082',
-            'commission': 1.00,
-            'notes': 'This is test a commission test 2',
-            'status': 'PENDING',
-            'currency':'USD'
+                    //Pass resulting correct body format through to Axios...
+                    axios({
+                        method: 'POST',
+                        url: 'https://www.refersion.com/api/manual_commission_credit',
+                        data: {
 
-        },
-        headers: {
-            'Content-Type': 'application/json',
-            'Refersion-Public-Key': process.env.pubKey,
-            'Refersion-Secret-Key':	process.env.secKey
-        }
-    }).then(function(response){
-        console.log(`This request is ${response.statusText} with a status code of ${response.status}`)
-        console.log(response.data)
-    });
+                            'id': id,
+                            'commission': commission,
+                            'notes': jsonObj[i].notes,
+                            'status': jsonObj[i].status,
+                            'currency': jsonObj[i].currency
+
+                        },
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Refersion-Public-Key': '',
+                            'Refersion-Secret-Key':	''
+                        }
+
+                    //Log resulting status to terminal
+                    }).then(function(response){
+                        console.log(`For ${id} the commission amount of ${commission} is ${response.statusText}`);
+
+                    //Use filesystem to log errors to a text file..    
+                    }).catch(function (error) {
+                        console.log(error);
+                        const results = JSON.stringify(error.response.config.data) + '\r\n';
+                        fs.appendFile('./logs/responselog.txt', `Error ${results}` , function (err) {
+                            if (err) throw err;
+                        })
+                    })
+                }
+            }))
 };
 
-orderTest();
-// commissionTest();
+// sendManualOrders();
+commissionTest();
